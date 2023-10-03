08b402feccf8fe892fde8d060478011612dd0d28
Kay2ser <kaysertony69@gmail.com>
2023-10-03 04:40:23 -0400

    hello python

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_fisrt_3 = word[:3], word_last_2 = word[:-2], middle_word[2:-8]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
