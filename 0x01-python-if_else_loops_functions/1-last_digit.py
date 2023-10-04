bbf6069125aadd38e98977da0d183f4327897e7d
Kay2ser <kaysertony69@gmail.com>
2023-10-04 17:48:24 -0400

    how are you  python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last = abs(number) % 10  # Calculate the last digit using absolute value
+print(f"The last digit of {number} is {last}{' and is greater than 5' if last > 5 else (' and is 0' if last == 0 else ' and is less than 6 and not 0')}")
+
 
